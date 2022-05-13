c8d34d688b46f4946dbf282d221dff7168f8c1b0
park sang hyeop <48915018+parksanghyup@users.noreply.github.com>
2022-05-13 15:11:36 +0900

    account model 작성완료

---
+++ src/main/java/com/rest/api/model/service/AccountService.java
@@ -0,0 +1,29 @@
+package com.rest.api.model.service;
+
+import com.rest.api.model.dto.AccountDto;
+import com.rest.api.model.mapper.AccountMapper;
+import com.rest.api.response.ApiResponse;
+import com.rest.api.response.ResponseMap;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountMapper accountMapper;
+
+    public ApiResponse addAccount(AccountDto accountDto) {
+        ResponseMap result = new ResponseMap();
+
+        accountDto.setPassword(hashPassword(accountDto.getPassword()));
+        accountMapper.insertAccount(accountDto);
+        return result;
+    }
+
+    private String hashPassword(String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
+
+}
